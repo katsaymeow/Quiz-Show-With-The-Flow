b3f2c9874c617732ea40eed76ba8120f57da973f
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-08-11 09:11:45 -0400

    trying to call functions and run the array of questions

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 
 ## Sources
 
-https://dev.to/sulaimonolaniran/building-a-simple-quiz-with-html-css-and-javascript-4elp
-This site has full code to inspect, but is lacking direcetions. 
--- assets/js/script1.js
+++ assets/js/script1.js
@@ -7,7 +7,7 @@ var questionsAsked = [
         choiceB: "in the title tag",
         choiceC: "in the head element with a link tag",
         choiceD: "in the footer element",
-        correctAnswer: "C"
+        correctAnswer: "in the head element with a link tag"
     },
     {
         question: "It is best practice to..",
@@ -31,17 +31,18 @@ var questionsAsked = [
         choiceB: "console.log() to see which funtions are working correctly",
         choiceC: "ask for help, you could have just stared at it to long",
         choiceD: "all of the above" ,
-        correctAnswer: "D"
+        correctAnswer: "all of the aboove"
 
     }
 ];
+//sets array to zero
 questionsAsked[0].question;
 questionsAsked[0].choiceA;
 questionsAsked[0].choiceB;
 questionsAsked[0].choiceC;
 questionsAsked[0].choiceD;
 questionsAsked[0].correctAnswer;
-
+//add var for que
 //create varible.
 var lastQuestionArray = questionsAsked.length - 1;
 var totalQuestionArray = 0;
@@ -59,27 +60,35 @@ function loadQuestions(){
     answerB.innerHTML = q.choiceB;
     answerC.innerHTML = q.choiceC;
     answerD.innerHTML = q.choiceD;
+    questionsAsked.forEach( {
+        question: "",
+        A: "",
+        B: "",
+        C: "",
+        D: "",
+        correctAnswer: ""
+    },
+     index, {
+        question: "",
+        A: "",
+        B: "",
+        C: "",
+        D: "",
+        correctAnswer: ""
+    } 
+    )
+    
+    // add an event listener to your answer buttons. When clicked we increase the totalQuestionArray++ and then we evaluate the answer, we can also load the next questions using loadQuestions
+    startBtn.addEventListener("click", userAnswer());
 }
-totalQuestionArray = 0;
-loadQuestions();
-totalQuestionArray++;
-loadQuestions();
-scoreOutput();
-timer();
+loadQuestions();//this need to finish loading next
+//HOW TO LISTEN FOR THE CORRECTANSWER???
+submitBtn.addEventListener("click", correctAnswer);
+// totalQuestionArray++;
+// loadQuestions();
+// scoreOutput();
+// timer();
 
-//post the score to the html
-function scoreOutput(){
-    for(var qArray = 0; qArray <= lastQuestionArray; qArray++){
-      score += "" + qArray; 
-    }//not sure if this will work
-};
-//functions for correct and incorrect answers
-function answerIsCorrect() {
-    document.getElementById(lastQuestionArray).style.backgroundColor = "green"
-}
-function answerIsWrong(){
-    document.getElementById(lastQuestionArray).style.backgroungColor = "red"
- }
 // this function is for the timer
 var time = 90;
 var wrongTime = (time - 10);
@@ -90,45 +99,48 @@ function timer() {
     if (wrongAnswerCount +1) timer() - wrongTime;
 }
 
-function userAnswer(answer){
-    if(questionsAsked[totalQuestionArray].correct == answer){
-        rightAnswerCount++;
-        answerIsCorrect;
-    } else {
-        wrongAnswerCount++;
-        answerIsWrong;
-    }
-    userAnswer();
-}
-
 var submitBtn = document.querySelector("click", "submit-button");
 var nextBtn = document.querySelector("click", "next-button");
-var startBtn = document.querySelector
-("click", "start-button");
-
-function startQuiz(){
-    startBtn.style.display = "none";
-    timer();
-    loadQuestions();
-    userAnswer();
-    scoreOutput();
-}
+// FINISH ME
+var startBtn = document.querySelector("click", "start-button");
+// startBtn.addEventListener("click", loadQuestions(), timer());
+// function 
 
 var rightAnswerCount = 0;
 var rightAnswer = document.getElementById("answer-right");
 var wrongAnswerCount = 0;
 var wrongAnswer = document.getElementById("answer-wrong");
-var score = 0;
 
-function scoreDisplay(){
-    rightAnswer.innerHTML = rightAnswerCount;
-    wrongAnswer.innerHTML = wrongAnswerCount;
+function scoreDisplay(answer){
+
+    if(questionsAsked.correct == answer){
+        rightAnswerCount++;
+        answerIsCorrect();
+        rightAnswer.innerHTML = rightAnswerCount;
+    } else {
+        wrongAnswerCount++;
+        answerIsWrong();
+        wrongAnswer.innerHTML = wrongAnswerCount;
+    }
+    scoreOutput();
+    answerIsCorrect();
+    answerIsWrong();
 }
+function answerIsCorrect() {
+    document.getElementById(lastQuestionArray).style.backgroundColor = "green"
+}
+function answerIsWrong(){
+    document.getElementById(lastQuestionArray).style.backgroungColor = "red"
+ }
+//post the score to the html
+function scoreOutput(){
+    for(var qArray = 0; qArray <= lastQuestionArray; qArray++){
+      score += "" + qArray; 
+    }//not sure if this will work
+};
+//functions for correct and incorrect answers
 
-// startBtn.addEventListener("click", startQuiz);
-
-
-console.log(userAnswer)
-console.log(startQuiz)
-console.log(timer)
-console.log(loadQuestions)
+// console.log(userAnswer)
+// console.log(startQuiz)
+// console.log(timer)
+// console.log(loadQuestions)
